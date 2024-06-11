553ed7bf5d2ac8f4b26b0ba68da8ec179fe40685
daz <daz@gradle.com>
2024-06-11 11:08:58 -0600

    Initial project

---
+++ src/main/java/com/example/maven_develocity_injection/MavenDevelocityInjectionApplication.java
@@ -0,0 +1,13 @@
+package com.example.maven_develocity_injection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenDevelocityInjectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenDevelocityInjectionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/maven_develocity_injection/MavenDevelocityInjectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.maven_develocity_injection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenDevelocityInjectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
